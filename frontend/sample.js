5f7c26e6e720da78e4be6823d504208fb1a081b3
manjumaikan <maikan007@gmail.com>
2021-02-19 08:45:59 +1100

    remove refs

--- frontend/sample.js
+++ frontend/sample.js
@@ -2,7 +2,6 @@
 
 //const API_HOST = process.env.API_HOST;
 const API_ROOT = `http://${process.env.API_HOST}:3000/api`;
-//const API_ROOT = `http://aws-realworld-api-dev.apps.sandpit.nbn-aws.local.:3000/api`;
 //var API_HOST = process.env.API_HOST || 'localhost';
 //const API_ROOT = `http://${API_HOST}:3000/api`;
 console.log(API_ROOT);
