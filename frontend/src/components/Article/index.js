c847e632f23704aaa5b30869f0bc36f57f6cf5e7
Manju Krishnappa <Manju.Krishnappa@mac6726.lan>
2022-05-18 14:29:10 +1000

    frontend npm update

--- frontend/src/components/Article/index.js
+++ frontend/src/components/Article/index.js
@@ -3,7 +3,7 @@ import CommentContainer from './CommentContainer';
 import React from 'react';
 import agent from '../../agent';
 import { connect } from 'react-redux';
-import marked from 'marked';
+import { marked } from 'marked';
 import { ARTICLE_PAGE_LOADED, ARTICLE_PAGE_UNLOADED } from '../../constants/actionTypes';
 
 const mapStateToProps = state => ({
@@ -94,4 +94,7 @@ class Article extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Article);
+export default connect(mapStateToProps, mapDispatchToProps)
+{
+  (Article);
+}
